6cbd2b4470df5511ccec8aee3d7a12696c4ae58d
ArthurdeBarbanson <arthurdebarbanson@gmail.com>
2016-08-03 00:10:58 +0200

    maj  .htaccess et erreur 404

--- index.php
+++ index.php
@@ -6,16 +6,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <meta name="author" content="Arthur de Barbanson">
-        <link rel="icon" href="private/images/agt-web-icone-4367.ico" />
+        <link rel="icon" href="images/agt-web-icone-4367.ico" />
 
         <title>Arthur de Barbanson</title>
 
         <!-- Bootstrap Core CSS -->
-        <link href="private/css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
 
         <!-- Custom CSS -->
-        <link href="private/css/agency.css" rel="stylesheet">
-        <link href="private/css/style.css" rel="stylesheet">
+        <link href="css/agency.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
     </head>
     <body id="page-top" class="index">
         <!-- Navigation -->
@@ -48,23 +48,23 @@
          include_once('modal.html'); ?>
 
         <!-- jQuery -->
-        <script src="private/js/jquery.js"></script>
+        <script src="js/jquery.js"></script>
 
         <!-- Bootstrap Core JavaScript -->
-        <script src="private/js/bootstrap.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
 
         <!-- Plugin JavaScript -->
         <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
 
         <!-- Contact Form JavaScript -->
-        <script src="private/js/jqBootstrapValidation.js"></script>
-        <script src="private/js/contact_me.js"></script>
+        <script src="js/jqBootstrapValidation.js"></script>
+        <script src="js/contact_me.js"></script>
 
         <!-- Custom Theme JavaScript -->
-        <script src="private/js/agency.js"></script>
+        <script src="js/agency.js"></script>
 
 <!--        svg3DTagCloud -->
-        <script src="private/js/jquery.svg3dtagcloud.min.js"></script>
-        <script src="private/js/script.js"></script>
+        <script src="js/jquery.svg3dtagcloud.min.js"></script>
+        <script src="js/script.js"></script>
     </body>
 </html>
